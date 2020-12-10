c33c6957397ea24aceaf7e9b2aee20eed3194d39
Lydia-tech <71572534+Lydia-tech@users.noreply.github.com>
2020-12-10 14:36:41 -0600

    added tests

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -14,6 +14,12 @@ test('creates a player object', () => {
   expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
 });
 
+test("gets player's health value", () => {
+  const player = new Player('Dave');
+
+  expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
 test("gets player's stats as an object", () => {
   const player = new Player('Dave');
 
@@ -23,6 +29,12 @@ test("gets player's stats as an object", () => {
   expect(player.getStats()).toHaveProperty('agility');
 });
 
+test('checks if player is alive or not', () => {
+  const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+})
+
 test('gets inventory from player or returns false', () => {
   const player = new Player('Dave');
 
@@ -31,4 +43,17 @@ test('gets inventory from player or returns false', () => {
   player.inventory = [];
 
   expect(player.getInventory()).toEqual(false);
+});
+
+test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999);
+
+    expect(player.health).toEqual(0);
 });
